95754bae4c858affc0c754bdc2b302a3aa517f60
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-09 10:16:57 +0330

    update: js script

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
+const api = "https://api.github.com/repos";
+const path = "contents/README.md";
+
 function getReadme(user, repo) {
-    fetch(`https://api.github.com/repos/${user}/${repo}/contents/README.md`) // Fetch the file from GitHub's api
-        .then(response => response.json())
+    fetch(`${api}/${user}/${repo}/${path}`) // Fetch the file from GitHub's api
+        .then((response) => {
+            response.json();
+        })
         .then(data => {
             const content = atob(data.content); // Convert from base64 to readable text
                   document.getElementById("readme-text").textContent = content; // Apply content to the document
-            console.log(content); // Log the content to the console
         })
-        .catch(error => console.log(error)); // Catch any errors
+        .catch((error) => {
+            console.log(error)
+        }); // Catch any errors
 }
 
 getReadme("amirhnajafiz", "amirhnajafiz");
